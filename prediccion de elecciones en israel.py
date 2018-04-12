77f53533304e84f376ef0035fedb8066b0095a27
deymarchow <campeonyed407@gmail.com>
2018-04-12 15:45:05 -0400

    Add files via upload

--- prediccion de elecciones en israel.py
+++ prediccion de elecciones en israel.py
@@ -6,24 +6,34 @@ from sklearn import cross_validation
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression ,LogisticRegression
 from sklearn.svm import SVC
-data=pd.read_csv('results_by_booth_2013 - english - v2.csv')
+D_israel=pd.read_csv('results_by_booth_2013 - english - v2.csv')
 
-X = data.drop('Registered_voters', axis = 1)
-Y = data['Registered_voters']
+
+names=list (D_israel)  #en esta linea obtengo los nombres o los titulos de cada columna y las almaceno en una variable
+
+column=D_israel.Balad     #en esta linea extraigo una columna de la base de datos de israel
+                        #la guardo en una variable en tipo ista.  
+
+
+p=column.remove('0')   #para esta linea deceo remover los " ceros" (0) de la columna seleccionada pero me lanza un error
+
+"""
+X = D_israel.drop(['settlement_code','booth_number','Registered_voters'], axis = 1)
+Y = D_israel['votes']
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.33, random_state = 5)
 
 lm = LinearRegression()
 lm.fit(X_train, Y_train)
-
 Y_pred = lm.predict(X_test)
 print (Y_pred)
 print (len(Y))
 print (len (Y_pred))
 
 plt.scatter(Y_test, Y_pred)
-plt.xlabel("Prices: $Y_i$")
-plt.ylabel("Predicted prices: $\hat{Y}_i$")
-plt.title("Prices vs Predicted prices: $Y_i$ vs $\hat{Y}_i$")
+plt.xlabel("votos_registrados")
+plt.ylabel("prediccion_de_votos")
+plt.title("elecciones de israel")
 plt.show()
 
+"""
